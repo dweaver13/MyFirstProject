c9aafa8116d10d74b3d09d7dc54dbdf0a3e88371
dweaver13 <69919211+dweaver13@users.noreply.github.com>
2020-08-31 14:36:08 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+/*
+Dale Weaver
+C++ Fall 2020
+Due : 31 August 2020
+Lab 1
+Lab description
+Our first lab. create a program that displays hello world and our favorite color
+*/
+
+#include <iostream>		
+
+int main()
+{
+	std::cout << "Hello World! My favorite color is purple.";
+
+	return 0;
+}
